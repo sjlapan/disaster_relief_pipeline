017aae066d6bbbb56f2ba1b59c24e553594ec3df
Stewart LaPan <stewartjlapan@gmail.com>
2021-01-16 16:27:26 -0700

    TYPE: Feature, chore
    Feature: built out code and notation for load_data() and clean_data() functions.
    Chore: Adjusted notation in jupyter notebook to indicate the need for altering my relative
    path when using create_engine.

--- data/process_data.py
+++ data/process_data.py
@@ -1,13 +1,71 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    PURPOSE: read in the data
+    INPUTS:
+        messages_filepath: string for path to messages.csv
+        categories_filepath: string for path to categories.csv
+    OUTPUT
+        df: a merged datframe using the common id column for 
+            messages and categories
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, left_on='id', right_on='id', how='left')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    PURPOSE: clean dataframe so it can be loaded into a SQL db
+    INPUT: 
+        df: the dataframe output of load_data()
+    OUTPUT:
+        df: the cleaned dataframe
+    STEPS:
+        1. Create dataframe of 36 categories by splitting string values
+        2. Extract column names from first row of category frame
+        3. Rename columns with extracted names
+        4. Strip values in category frame so that they are 
+            integer values (0 and 1)
+        5. Drop categories column from original dataframe
+        6. Concatenate original dataframe and categories dataframe
+        7. Drop duplicates
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(pat=';', expand=True)
 
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,].tolist()
+
+    # use this row to extract a list of new column names for categories.
+    category_colnames = [cat[:-2] for cat in row]
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    # convert categorie values to numbers (last character of each str)
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = [x.strip()[-1] for x in categories[column]]
+        
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+    
+    # drop the original categories column from `df`
+    df = df.drop(columns='categories', inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    return df
 
 def save_data(df, database_filename):
     pass  
